1419283e2bf2555a26bafa835c104e5bf30f6298
Maxwel <maxwellbakora@gmail.com>
2024-03-31 10:32:24 +0300

    My arithmetic lab

--- index.js
+++ index.js
@@ -1,2 +1,19 @@
-
 // Write your code here
+const num1 = 2;
+const num2 = 31;
+const multiply = num1 * num2;
+
+console.log(multiply);
+
+
+const random = Math.floor(Math.random() * Number.MAX_SAFE_INTEGER) + 1;
+console.log(random);
+
+let num3 = 16;
+let num4 = 6;
+const result = num3 %= num4;
+const mod = result;
+console.log(mod);
+
+const max = Math.max(2,7,12,14,17,16,19,20);
+console.log(max);
